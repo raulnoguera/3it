40e1b0e6f58c08f7a8a0e4e7de8ebad23ba82778
raulnoguerarod <raul.noguera@genesys.cl>
2023-07-19 14:30:45 -0400

    commit

---
+++ BackEnd/spring-boot-backend-apirest/src/test/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestControllerTest.java
@@ -0,0 +1,39 @@
+package com.bolsadeideas.springboot.backend.apirest.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ClienteRestControllerTest {
+
+	@Test
+	void testCreate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpload() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testVerFoto() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testListarRegiones() {
+		fail("Not yet implemented");
+	}
+
+}
